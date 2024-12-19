19a39ff8a38c21de3953f1c992c53d0b3bf18aa0
Preshiii <94832799+Preshiii@users.noreply.github.com>
2024-12-19 01:15:20 +0100

    Create start.sh

---
+++ start.sh
@@ -0,0 +1,12 @@
+if [ -z $UPSTREAM_REPO ]
+then
+  echo "Cloning main Repository"
+  git clone https://github.com/Preshiii/Corleone.git /vin_d
+else
+  echo "Cloning Custom Repo from $UPSTREAM_REPO "
+  git clone $UPSTREAM_REPO /Corleone
+fi
+cd /Corleone
+pip3 install -U -r requirements.txt
+echo "Starting Corleone...."
+python3 bot.py
